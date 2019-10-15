d10b6d50a30f617a608844d593258589a82d9dad
andrewchester1 <49591047+andrewchester1@users.noreply.github.com>
2019-10-14 19:12:10 -0500

    Intial Push

--- src/Driver.java
+++ src/Driver.java
@@ -1,20 +1,13 @@
+package investmentCalculator;
 
-public class Driver 
-{
-	public static void main(String[] args) 
+public class Driver
 	{
-		Investment a = new Investment();
-		Investment b = new Investment();
-		a.addContribution(new Contribution(1200, 1));
-		int month = 1;
-		for(int i = 0; i < 12; i++)
-		{
-			b.addContribution(new Contribution(100, month));
-		    month = month + 12;
-		}
-		
-		int totalInvestmentTime = 156;
-		System.out.println(a.getCurrentValue(totalInvestmentTime));
-		System.out.println(b.getCurrentValue(totalInvestmentTime));
+	   public static void main(String[] args)
+	   {
+	       Investment a = new Investment();
+	       a.addContribution();
+	       System.out.println(a.getTotalContributions());
+	       a.getCurrentValue();
 	}
-}
+	}
+
